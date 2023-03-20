d995cebe8a2507d6292adecf00301530f5509dbc
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2023-03-20 19:43:35 +0100

    added converting image to base 64

--- app/src/main/java/com/example/mobilesocialapp/fragments/CreateFragment.kt
+++ app/src/main/java/com/example/mobilesocialapp/fragments/CreateFragment.kt
@@ -1,15 +1,27 @@
 package com.example.mobilesocialapp.fragments
 
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Base64
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
 import com.example.mobilesocialapp.databinding.FragmentCreateBinding
+import java.io.ByteArrayOutputStream
+
 
 class CreateFragment : Fragment() {
     private var _binding: FragmentCreateBinding? = null
     private val binding get() = _binding!!
+    private lateinit var uri: Uri
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,6 +29,41 @@ class CreateFragment : Fragment() {
     ): View? {
         _binding = FragmentCreateBinding.inflate(inflater, container, false)
 
+        binding.imageButton.setOnClickListener() {
+            uploadImage(binding.imageView)
+        }
+
         return binding.root
+    }
+
+    private fun uploadImage(image: ImageView?) {
+        val intent = Intent()
+        intent.action = Intent.ACTION_GET_CONTENT
+        intent.type = "image/*"
+        startActivityForResult(intent, 1)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 1 && resultCode == Activity.RESULT_OK) {
+            binding.imageView.setImageURI(data?.data)
+
+            uri = data?.data!!
+            val input = activity?.contentResolver?.openInputStream(uri)
+            val image = BitmapFactory.decodeStream(input, null, null)
+
+            // Encode image to base64 string
+            val baos = ByteArrayOutputStream()
+            image?.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+            var imageBytes = baos.toByteArray()
+            val imageString = Base64.encodeToString(imageBytes, Base64.DEFAULT)
+
+            // Decode base64 string to image
+//            val imageBytesToDecode = Base64.decode(imageString, Base64.DEFAULT)
+//            val decodedImage = BitmapFactory.decodeByteArray(imageBytesToDecode, 0, imageBytes.size)
+//
+//            binding.imageView.setImageBitmap(decodedImage)
+        }
     }
 }
