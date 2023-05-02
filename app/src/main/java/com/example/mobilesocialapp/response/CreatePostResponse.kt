4b8774948bff92db1201e61bb3f54ca1a58dc126
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2023-05-02 16:31:28 +0200

    changed responses of creating and editing posts

--- app/src/main/java/com/example/mobilesocialapp/response/CreatePostResponse.kt
+++ app/src/main/java/com/example/mobilesocialapp/response/CreatePostResponse.kt
@@ -1,8 +1,5 @@
 package com.example.mobilesocialapp.response
 
 data class CreatePostResponse(
-    val message: String,
-    val username: String,
-    val creator: String,
-    val selectedFile: String
+    val message: String
 )
--- app/src/main/java/com/example/mobilesocialapp/response/EditPostResponse.kt
+++ app/src/main/java/com/example/mobilesocialapp/response/EditPostResponse.kt
@@ -1,8 +1,5 @@
 package com.example.mobilesocialapp.response
 
 data class EditPostResponse(
-    val message: String,
-    val username: String,
-    val creator: String,
-    val selectedFile: String
+    val message: String
 )
